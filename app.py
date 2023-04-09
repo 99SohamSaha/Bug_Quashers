0384f9dcea73a4aafc58d0dee83dbff93c938c30
Snehashish1609 <lahiri.sneh@gmail.com>
2023-04-09 12:17:01 +0530

    add more routes for csv upload and cluster

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import string
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.cluster import KMeans
+import os
 
 
 app = Flask(__name__)
@@ -12,7 +13,13 @@ app = Flask(__name__)
 # index route
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('buganalyzer.html')
+
+
+# cluster route
+@app.route('/cluster')
+def cluster():
+    return render_template('cluster.html')
 
 
 # master run API route
@@ -20,10 +27,12 @@ def index():
 def run():
     # TODO:
     # run model
-    model.train_clus_model()
+    err_count_arr, result = model.train_clus_model()
     # update global values
     # return required value
-    return
+    result = result.drop(['Clean data'], axis=1)
+    return result.to_json()
+
 
 # get error count from cluster number route
 @app.route('/api/v1.0/get-err-count/<cluster_no>')
@@ -32,6 +41,21 @@ def get_error_count(cluster_no):
     # dummy count
     error_count = int(cluster_no) + 10
     return str(error_count)
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    if request.method == "POST":
+        first_key = next(iter(request.files))
+        file = request.files[first_key]
+        if file.filename != "":
+            filename = file.filename
+            file.save(os.path.join("uploads", filename))
+            with open(os.path.join("uploads", filename), "r") as f:
+                contents = f.read()
+            return f"<h1>CSV file contents:</h1><pre>{contents}</pre>"
+    return "<h1>No file selected!</h1>"
+
 
 # driver function
 if __name__ == '__main__':
